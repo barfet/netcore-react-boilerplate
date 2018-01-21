dcea0aee85ae1357eaa83f2dbc3f6381fa59017d
RomanBorysenok <Roman_Borysenok@epam.com>
2018-01-21 19:51:28 +0000

    continue with components

---
+++ server/src/component/Planets/PlanetsComponentImpl.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+
+namespace Planets
+{
+    public interface PlanetsComponent
+    {
+        JToken GetAllPlanets();
+        JToken GetPlanetById(int id);
+    }
+}
